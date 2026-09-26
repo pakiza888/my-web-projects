259d7eef666ef6f0f25aaf9ccc7c9fad059d7541
Pakiza Munir <pakizamunir@gmail.com>
2026-09-26 10:47:15 -0700

    Updated website

---
+++ PYTHON FILES/lab task.py
@@ -0,0 +1,11 @@
+book={
+    "title": "The Great Gatsby",
+    "author": "F. Scott Fitzgerald",
+    "year": 1925,
+    "price":500
+}
+print(book.items())
+print(book["title"])
+
+book["in_stock"]= True
+book["price"]
